a61242ab7bfe66a9c1a7b439d144088c8f757340
ammar <ahmedammarnaseer@gmail.com>
2022-06-26 19:29:06 +0100

    add docstrings to gen and disc

--- src/discriminator.py
+++ src/discriminator.py
@@ -1,9 +1,31 @@
-import torch
-from torch import nn
+"""Discriminator implementation.
+
+This module defines the Discriminator to be used when constructing the Cycle GAN model.
+The Discriminator makes use of the ConvolutionBlock's defined here.
+
+"""
+
 from collections import OrderedDict
 
+import torch
+from torch import nn
+
 class ConvolutionBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride, instnorm):
+    """Performs a 2D convolution followed by an instance normalisation and a LeakyReLU activation on an image."""
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 stride: int,
+                 instnorm: bool) -> None:
+        """
+        Initialises the convolutional block (convolutional layer, instance normalisation, and LeakyReLU) instance.
+
+        Args:
+            in_channels: Number of channels in the input image.
+            out_channels: Desired number of output channels for the image.
+            stride: Stride to be used for the convolution.
+            instnorm: Boolean to decide whether to use instance normalisation or not.
+        """
         super(ConvolutionBlock, self).__init__()
         self.block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=stride, padding=1, padding_mode="reflect"),
@@ -13,12 +35,36 @@ class ConvolutionBlock(nn.Module):
             nn.LeakyReLU(negative_slope=0.2, inplace=True)
         )
 
-    def forward(self, x):
+    def forward(self,
+                x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass through the convolution block using an input image, x.
+
+        Args:
+            x: A torch tensor of the image to be passed through the convolutional block.
+
+        Returns:
+            A torch tensor of the output of the convolutional block.
+
+        """
         # print("conv", self.block(x).size())
         return self.block(x)
 
 class Discriminator(nn.Module):
-    def __init__(self, in_channels, inter_channels=64):  # inter_channels = 64
+    """The Discriminator used in the CycleGAN."""
+    def __init__(self,
+                 in_channels: int,
+                 inter_channels: int = 64) -> None:  # inter_channels = 64
+        """
+        Initialises the Discriminator.
+
+        The Discriminator is made up with four ConvolutionBlock's and an output 2D convolutional layer.
+
+        Args:
+            in_channels: Number of channels in the input image.
+            inter_channels: Number of channels (or filters) for output to be used with the first ConvolutionBlock.
+              This value will be multiplied by factors of two in the intermediate ConvolutionBlock's before the output.
+        """
         super(Discriminator, self).__init__()
         self.disc = nn.Sequential(OrderedDict([
             ('C64', ConvolutionBlock(in_channels, inter_channels, stride=2, instnorm=False)),  # 3 , 64
@@ -28,29 +74,48 @@ class Discriminator(nn.Module):
             ('out', nn.Conv2d(inter_channels * 8, 1, kernel_size=4, stride=1, padding=1, padding_mode="reflect"))
         ]))
 
-    def forward(self, x):
+    def forward(self,
+                x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass through the Discriminator using an input image, x.
+
+        Args:
+            x: A torch tensor of the image to be passed through the Discriminator.
+
+        Returns:
+            A torch tensor of the output from the Discriminator.
+
+        """
         return self.disc(x) # DO I NEED A SIGMOID HERE??
 
 if __name__ == "__main__":
-    from datasets import ImageDataset
-    import config
-    import os
-    from torchvision import transforms
 
-    target_shape = 256
-    tr = transforms.Compose([
-        transforms.RandomCrop(target_shape),
-        transforms.ToTensor()
-    ])
+    pass
+    # from datasets import ImageDataset
+    # import config
+    # import os
+    # from torchvision import transforms
     #
-    path = os.path.join(config.DATA_DIR, "horse2zebra")
-    dataset = ImageDataset(path=path, kind="test", transform=tr)
-    test_image = dataset[0][0]
-
-    rand_image2 = torch.rand(1, 3, 256, 256)
-    discc = Discriminator(3)
-    kk = discc(test_image)
-    print(kk.size())
+    # target_shape = 256
+    # tr = transforms.Compose([
+    #     transforms.RandomCrop(target_shape),
+    #     transforms.ToTensor()
+    # ])
+    # #
+    # path = os.path.join(config.DATA_DIR, "horse2zebra")
+    # dataset = ImageDataset(path=path, kind="test", transform=tr)
+    # test_image = dataset[0][0]
+    #
+    # convblock_test = ConvolutionBlock(3, 3, stride=2, instnorm=False)
+    # k = convblock_test.forward(test_image)
+    # print(type(k))
+    #
+    # #
+    # # rand_image2 = torch.rand(1, 3, 256, 256)
+    # discc = Discriminator(3)
+    # kk = discc(test_image)
+    # print(type(kk))
+    #
 
     # genny = Generator(3)
     # print("here")
--- src/generator.py
+++ src/generator.py
@@ -1,10 +1,33 @@
+"""Generator implementation.
+
+This module defines the Generator to be used when constructing the Cycle GAN model.
+The Generator makes use of the ConvolutionBlock, FractionalStridedConvBlock, and ResidualBlock defined here.
+"""
+
+from collections import OrderedDict
+
 import torch
 from torch import nn
-from collections import OrderedDict
 
 
 class ConvolutionBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+    """Performs a 2D convolution followed by an instance normalisation and a ReLU activation on an image."""
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: int,
+                 stride: int,
+                 padding: int) -> None:
+        """
+        Initialises the convolutional block (convolutional layer, instance normalisation, and ReLU) instance.
+
+        Args:
+            in_channels: Number of channels in the input image.
+            out_channels: Desired number of output channels for the image.
+            kernel_size: Kernel size to use for the convolution.
+            stride: Stride to be used for the convolution.
+            padding: Padding size to use for the convolution.
+        """
         super(ConvolutionBlock, self).__init__()
         self.block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, padding_mode="reflect"),
@@ -12,12 +35,35 @@ class ConvolutionBlock(nn.Module):
             nn.ReLU(inplace=True)
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass through the convolution block using an input image, x.
+
+        Args:
+            x: A torch tensor of the image to be passed through the convolutional block.
+
+        Returns:
+            A torch tensor of the output of the convolutional block.
+        """
         # print("conv", self.block(x).size())
         return self.block(x)
 
 class FractionalStridedConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride):
+    """Performs a 2D transposed convolution followed by an instance normalisation and a ReLU activation on an image."""
+    def __init__(self, in_channels: int,
+                 out_channels: int,
+                 kernel_size: int,
+                 stride: int) -> None:
+        """
+        Initialises the fractional strided convolutional block (convolutional layer, instance normalisation, and
+        ReLU) instance.
+
+        Args:
+            in_channels: Number of channels in the input image.
+            out_channels: Desired number of output channels for the image.
+            kernel_size: Kernel size to use for the convolution.
+            stride: Stride to be used for the convolution.
+        """
         super(FractionalStridedConvBlock, self).__init__()
         self.block = nn.Sequential(
             nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding=1, output_padding=1),
@@ -25,12 +71,32 @@ class FractionalStridedConvBlock(nn.Module):
             nn.ReLU(inplace=True)
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass through the fractional strided convolutional block using an input image, x.
+
+        Args:
+            x: A torch tensor of the image to be passed through the convolutional block.
+
+        Returns:
+            A torch tensor of the output of the convolutional block.
+        """
+        # print("conv", self.block(x).size())
         # print("FractionalStridedConvBlock", self.block(x).size())
         return self.block(x)
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    """A residual block to be used in the Discriminator."""
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int) -> None:
+        """
+        Initialises the residual block.
+
+        Args:
+            in_channels: Number of channels in the input image.
+            out_channels: Desired number of output channels for the image.
+        """
         super(ResidualBlock, self).__init__()
         self.residual = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, padding_mode="reflect"),
@@ -40,14 +106,39 @@ class ResidualBlock(nn.Module):
             nn.InstanceNorm2d(out_channels)
         )
 
-    def forward(self, x):
+    def forward(self,
+                x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass through the residual block using an input image, x.
+
+        Args:
+            x: A torch tensor of the image to be passed through the residual block.
+
+        Returns:
+            A torch tensor in which the original input image is added to the output of one forward pass through the
+            ResidualBlock.
+        """
         original_input = x.clone()
         k = original_input + self.residual(x)
         # print("Residual:", f"x size: {x.size()}", f"k size: {k.size()}")
         return original_input + self.residual(x)
 
 class Generator(nn.Module):
-    def __init__(self, in_channels, inter_channels=64):
+    """The Generator used in CycleGAN."""
+    def __init__(self,
+                 in_channels: int,
+                 inter_channels: int = 64) -> None:
+        """
+        Initialises the Generator.
+
+        The Generator is made up with ConvolutionBlock's, ResidualBlock's, FractionalStridedConvBlock's and an
+        output 2D convolutional layer.
+
+        Args:
+            in_channels: Number of channels in the input image.
+            inter_channels: Number of channels (or filters) for output to be used with the first ConvolutionBlock.
+              This value will be scaled in the intermediate layers before the output.
+        """
         super(Generator, self).__init__()
         self.gen = nn.Sequential(OrderedDict([
             ('c7s1-64', ConvolutionBlock(in_channels, inter_channels, 7, 1, 3)),  # 3, 64
@@ -68,12 +159,23 @@ class Generator(nn.Module):
             # in_channels, out_channels, kernel_size, stride)
         ]))
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass through the Generator using an input image, x.
+
+        Args:
+            x: A torch tensor of the image to be passed through the Generator.
+
+        Returns:
+            A torch tensor in which the original input image has been through one forward pass after which a non-linear
+            activation of tanh is applied to it.
+        """
         X = self.gen(x)
         # print(torch.tanh(X).size())
         return torch.tanh(X)
 
 if __name__ == "__main__":
+    pass
     from datasets import ImageDataset
     import config
     import os
@@ -88,15 +190,25 @@ if __name__ == "__main__":
     path = os.path.join(config.DATA_DIR, "horse2zebra")
     dataset = ImageDataset(path=path, kind="test", transform=tr)
     test_image = dataset[0][0]
+
+
+
     # in_channels = dataset[0][0].size()[0]
     #
-    # convblock = ConvolutionBlock(in_channels, 3, kernel_size=3, stride=2)
-    # new_image = convblock.forward(test_image)
+    convblock = ConvolutionBlock(3, 3, kernel_size=3, stride=2, padding=1)
+    new_image = convblock.forward(test_image)
+    print(type(new_image))
+    print("----")
+    inter_channels = 64
+    fake = torch.rand(inter_channels * 4, 256, 256)
+    fractional = FractionalStridedConvBlock(inter_channels * 4, inter_channels * 2, 3, 2)
+    print(type(fractional.forward(fake)))
     # print(new_image.size())
     #
-    # # resblock = ResidualBlock(new_image.size()[0], 3, kernel_size=1, stride=1)
-    # # res = resblock.forward(new_image)
-    # # print(res)
+    resblock = ResidualBlock(new_image.size()[0], 3)
+    res = resblock.forward(new_image)
+    print("res")
+    print(res)
     #
     # inpprocess = InputOutputProcessing(in_channels, out_channels=64)
     # img_ready = inpprocess(test_image)
@@ -115,11 +227,12 @@ if __name__ == "__main__":
     # imgg = tr(ki)
     # imgg.show()
 
-    rand_image2 = torch.rand(1, 3, 256, 256)
-    genny = Generator(3)
-    print("here")
-    print(test_image.size())
-    outss = genny(test_image)
+    # rand_image2 =
+    # genny = Generator(3)
+    # genny.forward(test_image)
+    # print("here")
+    # print(test_image.size())
+    # outss = genny(test_image)
     # import torchvision.transforms as transforms
     # tr = transforms.ToPILImage()
     # imgg = tr(outss)
