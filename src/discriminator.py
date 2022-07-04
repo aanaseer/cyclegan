838507c95495c6f51e40ae87f6274095ec66464e
ammar <ahmedammarnaseer@gmail.com>
2022-07-04 01:20:45 +0100

    clean discriminator

--- src/discriminator.py
+++ src/discriminator.py
@@ -65,10 +65,10 @@ class Discriminator(nn.Module):
         """
         super(Discriminator, self).__init__()
         self.disc = nn.Sequential(OrderedDict([
-            ('C64', ConvolutionBlock(in_channels, inter_channels, stride=2, instnorm=False)),  # 3 , 64
-            ('C128', ConvolutionBlock(inter_channels, inter_channels * 2, stride=2, instnorm=True)),  # 64, 128
-            ('C256', ConvolutionBlock(inter_channels * 2, inter_channels * 4, stride=2, instnorm=True)),  # 128, 256
-            ('C512', ConvolutionBlock(inter_channels * 4, inter_channels * 8, stride=1, instnorm=True)),  # 256, 512
+            ('C64', ConvolutionBlock(in_channels, inter_channels, stride=2, instnorm=False)),
+            ('C128', ConvolutionBlock(inter_channels, inter_channels * 2, stride=2, instnorm=True)),
+            ('C256', ConvolutionBlock(inter_channels * 2, inter_channels * 4, stride=2, instnorm=True)),
+            ('C512', ConvolutionBlock(inter_channels * 4, inter_channels * 8, stride=1, instnorm=True)),
             ('out', nn.Conv2d(inter_channels * 8, 1, kernel_size=4, stride=1, padding=1, padding_mode="reflect"))
         ]))
 
@@ -83,39 +83,4 @@ class Discriminator(nn.Module):
             A torch tensor of the output from the Discriminator.
 
         """
-        return self.disc(x)  # DO I NEED A SIGMOID HERE??
-
-
-if __name__ == "__main__":
-
-    pass
-    # from datasets import ImageDataset
-    # import config
-    # import os
-    # from torchvision import transforms
-    #
-    # target_shape = 256
-    # tr = transforms.Compose([
-    #     transforms.RandomCrop(target_shape),
-    #     transforms.ToTensor()
-    # ])
-    # #
-    # path = os.path.join(config.DATA_DIR, "horse2zebra")
-    # dataset = ImageDataset(path=path, kind="test", transform=tr)
-    # test_image = dataset[0][0]
-    #
-    # convblock_test = ConvolutionBlock(3, 3, stride=2, instnorm=False)
-    # k = convblock_test.forward(test_image)
-    # print(type(k))
-    #
-    # #
-    # # rand_image2 = torch.rand(1, 3, 256, 256)
-    # discc = Discriminator(3)
-    # kk = discc(test_image)
-    # print(type(kk))
-    #
-
-    # genny = Generator(3)
-    # print("here")
-    # print(test_image.size())
-    # outss = genny(test_image)
+        return self.disc(x)
