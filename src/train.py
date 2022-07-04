00b41950b414827b2651a11a9e0b2bd7f1d1b185
ammar <ahmedammarnaseer@gmail.com>
2022-07-04 02:25:26 +0100

    train clean up

--- src/train.py
+++ src/train.py
@@ -9,22 +9,18 @@ PROJ_ROOT = os.path.abspath(os.path.join(os.pardir))
 DATA_DIR = os.path.join(PROJ_ROOT, "data")
 SAVE_DIR = os.path.join(PROJ_ROOT, "outputs")
 
-PROJ_ROOT = "/home/naseer/cyclegan-mmsc-special-topic/"
-DATA_DIR = "/scratch/naseer/"
-SAVE_DIR = "/home/naseer/cyclegan-mmsc-special-topic/outputs/"
-
 config = {
-    "dataset_name": "horse2zebra",
+    "dataset_name": "bit2moji2simpsons",
     "image_dim": 256,
     "image_channels": 3,
-    "learning_rate": 0.0002,
-    "beta1": 0,
+    "learning_rate": 0.002,
+    "beta1": 0.5,
     "beta2": 0.999,
     "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
     "batch_size": 1,
     "lambda_cycle": 10,
     "lambda_identity": 0.0001,
-    "condition_step": 100,
+    "condition_step": 250,
     "save_checkpoint": True,
     "save_images": True,
     "epochs": 200,
@@ -33,8 +29,7 @@ config = {
     "proj_root": PROJ_ROOT,
     "data_dir": DATA_DIR,
     "output_save_dir": SAVE_DIR,
-    "learning_decay": False
-
+    "learning_decay": True
 }
 
 def main():
